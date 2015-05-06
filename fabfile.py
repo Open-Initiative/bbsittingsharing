f414069dbf90073653b0bce2f01385652ed235f5
Sylvain Le Bon <lebon.sylvain@gmail.com>
2015-05-06 17:46:42 +0200

    update: production preparation

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+from __future__ import with_statement
+from fabric.api import local, settings, abort, run, cd, env
+from fabric.contrib.console import confirm
+
+env.hosts = ['bbsittingsharing@ssh.alwaysdata.com']
+
+def deploy():
+    with cd('bbsittingsharing'):
+        run("git pull origin master")
+        run("git merge master")
+        run("./manage.py syncdb")
+        run("./manage.py collectstatic")
