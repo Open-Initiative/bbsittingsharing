2dd9d0f7a3f746e09e25db16514b439c5b56b738
Sylvain Le Bon <lebon.sylvain@gmail.com>
2015-07-06 15:19:05 +0200

    bugfix: fixed default context

--- bbsittingsharing/helpers.py
+++ bbsittingsharing/helpers.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 from django.core.urlresolvers import set_script_prefix
 from django.core.mail import EmailMultiAlternatives
-from django.template import RequestContext
+from django.template import Context, RequestContext
 from django.template.loader import get_template
 
-def send_email(recipients, subject, template_name, email_context={}):
+def send_email(recipients, subject, template_name, email_context=Context()):
     text_content = get_template('notification/%s.txt'%template_name).render(email_context)
     html_content = get_template('notification/%s.html'%template_name).render(email_context)
     msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, recipients)
